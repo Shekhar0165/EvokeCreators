92f01761d1c152375f51a13879c0058bebfd1c04
shekhar kashyap <shekharkashyap913@gmail.com>
2025-02-28 15:59:36 +0530

    add google aya

--- src/app/layout.js
+++ src/app/layout.js
@@ -56,7 +56,22 @@ export default function RootLayout({ children }) {
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
-        
+
+        {/* Google Analytics Script */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9P5LRMCWZS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9P5LRMCWZS');
+          `}
+        </Script>
+
         <ThemeProvider>
           <Navbar />
           {children}
